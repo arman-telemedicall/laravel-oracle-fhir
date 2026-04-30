1fc4ff9f572521d79bb8bf9f5bb724b10befc2f8
Arman <arman@telemedicall.com>
2026-04-30 23:30:27 +0300

    Update OracleFhirFhirClientInterface.php

--- src/Contracts/OracleFhirFhirClientInterface.php
+++ src/Contracts/OracleFhirFhirClientInterface.php
@@ -7,4 +7,7 @@ interface OracleFhirFhirClientInterface
 	public function getPatientById(string $clientId, string $tenantId, string $patientId): string;
 	
 	public function getObservationsList(string $clientId, string $tenantId, string $patientId): string;
+
+	/** @param array<string, mixed> $patientData */
+    public function patientCreateSystem(string $clientId, string $tenantId, array $patientData): array;
 }
